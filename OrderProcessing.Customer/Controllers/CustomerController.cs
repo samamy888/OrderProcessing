e845df9414e92bbd6fadedc67d0c41f8444e3c0a
wenhui <samamy888@gmail.com>
2022-07-05 11:01:46 +0800

    git init

---
+++ OrderProcessing.Customer/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderProcessing.Customer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerRepository _customerRepository;
+        public CustomerController(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Customer>>> GetAllCustomers()
+        {
+            return await _customerRepository.GetAllCustomers();
+        }
+    }
+}
---
+++ OrderProcessing.Customer/CustomerRepository.cs
@@ -0,0 +1,29 @@
+﻿namespace OrderProcessing.Customer
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly List<Customer> customers = new();
+        public CustomerRepository()
+        {
+            customers.Add(new Customer()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Joydip",
+                LastName = "Kanjilal",
+                EmailAddress = "joydipkanjilal@yahoo.com"
+            });
+
+            customers.Add(new Customer()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Steve",
+                LastName = "Smith",
+                EmailAddress = "stevesmith@yahoo.com"
+            });
+        }
+        public Task<List<Customer>> GetAllCustomers()
+        {
+            return Task.FromResult(customers);
+        }
+    }
+}
